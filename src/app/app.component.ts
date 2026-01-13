39e4112454d2c09cfee305442d246d942463387b
Velin Dankov <velin80@web.de>
2026-01-13 12:29:06 +0200

    A simple todo is created!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { TodoService } from "./todo.service.js";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
+  todoService = inject(TodoService);
+  newText = "";
+
+  addTodo() {
+    this.todoService.addTodo(this.newText);
+    this.newText = "";
+  }
 }
